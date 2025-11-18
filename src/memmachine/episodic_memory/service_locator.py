b21d4a83a19bfa4e545af16a787205c8a31cd4f2
shu wang <shu.wang@memverge.com>
2025-11-18 01:06:32 +0000

    Fix EpisodicMemoryParam problem

--- src/memmachine/episodic_memory/service_locator.py
+++ src/memmachine/episodic_memory/service_locator.py
@@ -36,7 +36,7 @@ async def epsiodic_memory_params_from_config(
 
     return EpisodicMemoryParams(
         session_key=config.session_key,
-        metrics_factory=resource_manager.get_metrics_factory(
+        metrics_factory=await resource_manager.get_metrics_factory(
             config.metrics_factory_id,
         ),
         long_term_memory=long_term_memory,
